e5a77a695bb012fdebff7aa3c122246bfe81f68e
Milena Cicha <milenacicha@yahoo.co.uk>
2018-09-17 19:13:14 +0200

    Dodanie mozliwosci ponownej gry

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -15,32 +15,44 @@ return guess;
 
 
 int main(){
-    srand ((int) time(NULL));
-    int randomNumber = rand() % 100 + 1;
-    std::cout << "Wylosowano liczbe z przedzialu 1-100." << std::endl;
 
 
-    std::cout << "Masz 10 szans na odgadniecie liczby.\n";
-    userPrompt();
     int tries = 1;
 
-    while (guess != randomNumber && tries <10) {
-        if (guess < randomNumber)
-        std::cout << "Liczba jest za mala.";
+    char goAgain = 'Y';
+
+
+    while(goAgain == 'Y') {
+        std::cout << "Wylosowano liczbe z przedzialu 1-100.\n";
+        srand ((int) time(NULL));
+        int randomNumber = rand() % 100 + 1;
+        std::cout << "Masz 10 szans na odgadniecie liczby.\n";
+        userPrompt();
+        while (guess != randomNumber && tries < 10) {
+            if (guess < randomNumber)
+                std::cout << "Liczba jest za mala.";
+            else
+                std::cout << "Liczba jest za duza.";
+
+            std::cout << " To byla " << tries << " proba. Sprobuj jeszcze raz.\n";
+            userPrompt();
+            tries += 1;
+
+
+        }
+        if (tries <= 10 && guess == randomNumber)
+            std::cout << "Wygrales! Udalo Ci sie zgadnac za " << tries << " razem.\n";
         else
-        std::cout << "Liczba jest za duza.";
-
-    std::cout <<" To byla "<<tries<<" proba. Sprobuj jeszcze raz.\n";
-    userPrompt();
-    tries +=1;
-
+            std::cout << "Wykorzystales wszystkie proby. Poszukiwana liczba bylo " << randomNumber << ".\n";
+        std::cout<<"Czy chcesz zagrac ponownie (Y/N)?\n";
+        std::cin>>goAgain;
+        goAgain = toupper(goAgain);
+        tries = 1;
 
 
     }
-    if(tries < 10)
-    std::cout << "Wygrales! Udalo Ci sie zgadnac za "<<tries<<" razem.\n";
-    else
-    std::cout<<"Wykorzystales wszystkie proby. Poszukiwana liczba bylo "<<randomNumber<<".\n";
+
+    std::cout<<"Dziekuje za gre!\n";
 
 return 0;
 }
